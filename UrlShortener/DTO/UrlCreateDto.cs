c5630c783184af579d937470820f66e6a060210d
DESKTOP-V50OBM3\37529 <Denis Gavrilkov>
2025-06-15 20:37:18 +0300

    completed task

---
+++ UrlShortener/DTO/UrlCreateDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrlShortener.DTO
+{
+    public class UrlCreateDto
+    {
+        [Required]
+        [Url(ErrorMessage = "Введите корректный URL.")]
+        public string OriginalUrl { get; set; } = string.Empty;
+    }
+
+}
--- UrlShortener/Program.cs
+++ UrlShortener/Program.cs
@@ -1,11 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using UrlShortener.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<UrlShortenerDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+);
 
 var app = builder.Build();
 
@@ -17,9 +24,16 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
+app.UseDefaultFiles(); 
 app.UseAuthorization();
 
 app.MapControllers();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<UrlShortenerDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
